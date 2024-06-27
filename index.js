df30c39fe5bd0eeba5d5264b2798632bfd3bce4d
DmitriyAngve <kirirch129@gmail.com>
2024-06-27 13:49:31 +0300

    #

--- index.js
+++ index.js
@@ -18,12 +18,54 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-scene.add(hemiLight);
+// Create a line geometry from the spline
+const points = spline.getPoints(100);
+// BufferGeometry - это основной класс для представления геометрических форм в threejs. Используется для хранения points, faces, normals, colors и тд
+// setFromPoints - это метод класса BufferGeometry, который позволяет создать геометрию на основе массива точек
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+const line = new THREE.Line(geometry, material);
+// scene.add(line);
+
+// create edges geometry from the spline
+const edges = new THREE.EdgesGeometry(geometry, 0);
+const lineMat = new THREE.LineBasicMaterial({ color: 0xffffff });
+const tubeLines = new THREE.LineSegments(edges, lineMat);
+scene.add(tubeLines);
+
+// create a tube geometry from the spline
+// 222 - количество сегментов, на которые делится spline
+// 0.65 - радиус трубы
+// 16 - количество радиальных сегментов (детализация поперечного сечения трубы)
+// true - замкнута ли кривая
+// side: THREE.DoubleSide - заставляет материал отображаться с обеих сторон поверхности (труба будет видна как снаружи, так и внутри)
+// wireframe - эта настройка делает материал отображаемым в режиме каркаса (отображает ребра геометрии)
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 0.65, 16, true);
+const tubeMat = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  // side: THREE.DoubleSide,
+  wireframe: true,
+});
+const tube = new THREE.Mesh(tubeGeo, tubeMat);
+scene.add(tube);
+
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+// scene.add(hemiLight);
+
+function updateCamera(t) {
+  const time = t * 0.1;
+  const looptime = 8 * 1000;
+  const p = (time % looptime) / looptime;
+  const pos = tubeGeo.parameters.path.getPointAt(p);
+  const lookAt = tubeGeo.parameters.path.getPointAt((p + 0.03) % 1);
+  camera.position.copy(pos);
+  camera.lookAt(lookAt);
+}
 
 function animate(t = 0) {
   requestAnimationFrame(animate);
 
+  updateCamera(t);
   renderer.render(scene, camera);
   controls.update();
 }
